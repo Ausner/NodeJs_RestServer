c6e6889baa79ff6d685faf7203d787736103e31a
Ausner <ausnermiranda@gmail.com>
2021-05-27 18:33:53 -0600

    Usuarios CRUD listo 27-05-2021

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,18 +1,44 @@
 const { Router } = require('express');
-const { usuariosGet, usuariosPost, usuariosPut, usuariosPatch, usuariosDelete } = require('../controllers/usuarios');
-
+const { check } = require('express-validator');
+const {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+} = require('../controllers/usuarios');
+const { esRoleValido, emailExiste, existeUsuarioPorId } = require('../helpers/db-validators');
+const validarCampos = require('../middlewares/validar-campos');
 const router = Router();
 
 
 router.get('/', usuariosGet);
 
-router.post('/', usuariosPost)
+router.post('/', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(), //Valida si viene el nombre
+    check('password', 'La contraseña es obligatoria y debe ser más de 6 letras').isLength({ min: 5 }), //Valida si el password tiene minimo 6 caracteres
+    //check('correo', 'El correo no es válido').isEmail(), //Validando que el correo sea validator
+    check('correo').custom(emailExiste),
+    //check('rol', 'No es un rol permitido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom(esRoleValido),
+    validarCampos
+], usuariosPost)
 
-router.put('/:id', usuariosPut);
+
+router.put('/:id', [
+    check('id', 'No es un id valido').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    check('rol').custom(esRoleValido),
+    validarCampos
+], usuariosPut); //Valida si es un mongoID
 
 router.patch('/', usuariosPatch);
 
-router.delete('/', usuariosDelete);
+router.delete('/:id', [
+    check('id', 'No es un id valido').isMongoId(),
+    check('id').custom(existeUsuarioPorId),
+    validarCampos
+], usuariosDelete);
 
 
 
